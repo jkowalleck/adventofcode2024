1107ba17aa0ff736c26e41f91d8971eca464986b
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-02 13:52:57 +0100

    02
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- d02/input.py
+++ d02/input.py
@@ -4,7 +4,6 @@ import re
 from os import path
 
 report = []
-pattern = re.compile(r'^(\d+) +(\d+)$')
 with open(path.join(path.dirname(__file__), 'input.txt'), 'r') as f:
     for line in f.readlines():
         report.append(tuple(map(int, line.split())))
