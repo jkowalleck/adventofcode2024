25c906262c953034f911e97e2aa64ef09fdf189d
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-03 18:44:33 +0100

    03
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- d03/p2.py
+++ d03/p2.py
@@ -3,16 +3,14 @@ from re import compile
 from input import lines
 
 
-_INSTR_PATTERN = compile(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)")
+_INSTR_PATTERN = compile(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do|don't)\(\)")
 
 muls = []
 enable = True
 for line in lines:
-  for mul, a, b, do, dont in _INSTR_PATTERN.findall(line):
+  for mul, a, b, do in _INSTR_PATTERN.findall(line):
     if do:
-      enable = True
-    elif dont:
-      enable = False
+      enable = do == 'do'
     elif mul:
       if enable:
         muls.append((int(a), int(b)))
