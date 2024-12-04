2983d0d0067e836ed418a5060ae3a422621bc09a
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-04 21:47:33 +0100

    04
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ 04/p2.py
@@ -0,0 +1,25 @@
+from input import lines
+
+res = 0
+
+ls = len(lines)
+cs = len(lines[0])
+for l in range(1, ls -1 ):
+  for c in range(1, cs -1):
+    if lines[l][c] == 'A':
+      try:
+        a1, a2, b1, b2 = lines[l - 1][c - 1], lines[l + 1][c + 1], lines[l - 1][c + 1], lines[l + 1][c - 1]
+      except IndexError:
+        continue
+      if (
+        (a1 == 'M' and a2 == 'S')  # \ MAS
+        or
+        (a1 == 'S' and a2 == 'M')  # \ SAM
+      ) and (
+        (b1 == 'M' and b2 == 'S')  # / MAS
+        or
+        (b1 == 'S' and b2 == 'M')  # / SAM
+      ):
+        res += 1
+
+print('res', res)
