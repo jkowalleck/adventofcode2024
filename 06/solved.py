9ae62723f292ee83d25c58743cde8d91a5787bc1
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-07 00:17:02 +0100

    06
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 06/solved.py
+++ 06/solved.py
@@ -4,45 +4,35 @@ import math
 
 i = open("../input.txt").read()
 
+
 class Direction(Enum):
   U = "^"
   R = ">"
   D = "v"
   L = "<"
 
+
 width = len(i.splitlines()[0])
 height = len(i.splitlines())
 checkfield = i.replace("\n", "")
 
-y = math.floor(checkfield.index("^") / width)
-x = checkfield.index("^") % width
-
-step_forward = {
-  Direction.U: lambda x,y: (x,y-1),
-  Direction.R: lambda x,y: (x+1,y),
-  Direction.D: lambda x,y: (x,y+1),
-  Direction.L: lambda x,y: (x-1,y)
-}
-
 field = []
-
 for line in i.splitlines():
   field.append(list(line))
 
+step_forward = {
+  Direction.U: lambda x, y: (x, y - 1),
+  Direction.R: lambda x, y: (x + 1, y),
+  Direction.D: lambda x, y: (x, y + 1),
+  Direction.L: lambda x, y: (x - 1, y)
+}
 
-d = Direction(field[y][x])
 
 def render(field):
   os.system("clear")
   for line in field:
     print(*line, sep="", end="\n")
 
-can_step = {
-  Direction.U: lambda x,y: field[x][y-1] != "#",
-  Direction.R: lambda x,y: field[x+1][y] != "#",
-  Direction.D: lambda x,y: field[x][y+1] != "#",
-  Direction.L: lambda x,y: field[x-1][y] != "#"
-}
 
 rot = {
   Direction.U: Direction.R,
@@ -51,42 +41,48 @@ rot = {
   Direction.L: Direction.U
 }
 
-def find_loop(field,x,y,d):
+
+def find_loop(field, x, y, d):
   visited = set()
   while True:
-    visited.add((x,y,d))
+    visited.add((x, y, d))
 
-    #input()
-    #render(field)
-    sx,sy = step_forward[d](x,y)
+    # input()
+    # render(field)
+    sx, sy = step_forward[d](x, y)
 
     if sx < 0 or sy < 0 or sx >= width or sy >= height:
       field[y][x] = "X"
-      #render(field)
+      # render(field)
       return False, visited
 
     field[y][x] = "X"
 
-    while field[sy][sx] in ["#","O"]:
+    while field[sy][sx] in ["#", "O"]:
       d = rot[d]
-      sx,sy = step_forward[d](x,y)
+      sx, sy = step_forward[d](x, y)
 
-    x,y = sx,sy
+    x, y = sx, sy
 
     field[y][x] = d.value
-    if (x,y,d) in visited:
+    if (x, y, d) in visited:
       return True, visited
+
+
+y = math.floor(checkfield.index("^") / width)
+x = checkfield.index("^") % width
+d = Direction(field[y][x])
 
 total = 0
 newfield = [l[::] for l in field]
-_, pathmap = find_loop(newfield,x,y,d)
+_, pathmap = find_loop(newfield, x, y, d)
 pathmap = {(x[0], x[1]) for x in pathmap}
 print("soviele: ", len(pathmap))
-for j,i in pathmap:
+for j, i in pathmap:
   if field[i][j] != "#":
     newfield = [l[::] for l in field]
     newfield[i][j] = "O"
-    if find_loop(newfield,x,y,d)[0]:
+    if find_loop(newfield, x, y, d)[0]:
       total += 1
 
 print(total)
