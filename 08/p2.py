a17fbff1842232f7326dd2028b9e4ddf7224fd42
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-08 15:56:51 +0100

    08
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 08/p2.py
+++ 08/p2.py
@@ -16,7 +16,7 @@ for each pair of emitters:
 # rule 3: antinodes koenen nicht auf der pos von emitter sliegen
 # result: anzahl der "unique" positionen von antinodes + "unique" position von emittern
 
-field = inp('arm')
+field = inp('sample')
 height = len(field)
 width = len(field[0])
 
@@ -29,6 +29,12 @@ for y, l in enumerate(field):
       else:
         emitters[c].add((x, y))
 
+
+def in_bounds(x, y):
+  return (0 <= x < width) \
+    and (0 <= y < height)
+
+
 antinodes = set(chain.from_iterable(e for _, e in emitters.items()))
 for et, es in emitters.items():
   for e1, e2 in combinations(es, 2):
@@ -36,20 +42,15 @@ for et, es in emitters.items():
     x2, y2 = e2
     xd = x1 - x2
     yd = y1 - y2
-    y = y1
-    for x in range(x1, -1 if xd < 0 else width, xd):
-      antinodes.add((x, y))
-      y = y + yd
-      if not (0 <= y < height):
-        break
-    xd *= -1
-    yd *= -1
-    y = y1
-    for x in range(x1, -1 if xd < 0 else width, xd):
-      antinodes.add((x, y))
-      y = y + yd
-      if not (0 <= y < height):
-        break
 
-valids = sorted(tuple(antinodes))
-print('res', len(valids))
+    for s in (+1, -1):
+      _xd = s * xd
+      _yd = s * yd
+      y = y1
+      x = x1
+      while in_bounds(x, y):
+        antinodes.add((x, y))
+        x += _xd
+        y += _yd
+
+print('res', len(antinodes))
