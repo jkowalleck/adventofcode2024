3f9ea86bd1239aca3fd323a000eb811c22825f0e
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-10 21:04:45 +0100

    10
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 10/input.py
+++ 10/input.py
@@ -6,6 +6,6 @@ def inp(f):
   lines = []
   with open(path.join(path.dirname(__file__), f'{f}.txt'), 'r') as f:
     for line in f.readlines():
-       lines.append(line.strip())
+       lines.append(tuple(int(c) for c in line.strip()))
   return lines
 
---
+++ 10/p2.py
@@ -0,0 +1,47 @@
+from itertools import pairwise
+
+from input import inp
+
+map = inp('kow')
+height = len(map)
+width = len(map[0])
+
+dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))
+
+
+def in_bounds(x, y):
+  return (0 <= x < width) \
+    and (0 <= y < height)
+
+
+tops = {
+  (x, y): 0
+  for y, line in enumerate(map)
+  for x, char in enumerate(line)
+  if char == 9
+}
+
+
+def foo(cur, start):
+  cx, cy = cur
+  c = map[cy][cx]
+  for xd, yd in dirs:
+    x, y = cx + xd, cy + yd
+    if not in_bounds(x, y):
+      continue  # next dir
+    if map[y][x] != c + 1:
+      continue  # next dir
+    next = (x, y)
+    if map[y][x] == 9:
+      tops[next] += 1
+    else:
+      foo(next, start)
+
+
+for y, line in enumerate(map):
+  for x, char in enumerate(line):
+    if char == 0:
+      start = (x, y)
+      foo(start, start)
+
+print('res', sum(tops.values()))
