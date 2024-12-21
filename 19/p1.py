1a648e9274e39b61f2ed61b1b2653846792701e6
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-21 12:57:26 +0100

    19
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ 19/p1.py
@@ -0,0 +1,24 @@
+import re
+
+from input import inp
+
+have, _, *wants = inp('kow')
+
+have_opt = set(have.split(', '))
+for h in tuple(have_opt):
+    have_opt.remove(h)
+    pat = re.compile(f'^(:?{"|".join(have_opt)})+$')
+    if pat.match(h) is None:
+        have_opt.add(h)
+
+pat = re.compile(f'^(:?{"|".join(have_opt)})+$')
+
+print('have_opt:', len(have_opt), sorted(have_opt))
+print('pat:', pat)
+
+cnt = 0
+for w, want in enumerate(wants):
+    print(w)
+    if pat.match(want) is not None:
+        cnt += 1
+print('cnt:', cnt)
