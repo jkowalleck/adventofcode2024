6163728196f107875e67d38de65635bdca171895
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-25 10:25:52 +0100

    22
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 22/p2.py
+++ 22/p2.py
@@ -2,7 +2,6 @@ from collections import deque
 from itertools import chain
 
 
-
 def mrand(salt):
   secret = salt
   while True:
@@ -12,9 +11,10 @@ def mrand(salt):
     yield secret
 
 
-secrets = map(int, open('sample2.txt').read().split('\n'))
+secrets = map(int, open('kow.txt').read().split('\n'))
 prices_seqs = []
-for secret in secrets:
+for s, secret in enumerate(secrets):
+  print('process secret', s)
   _prices_seqs = {}
   prices_seqs.append(_prices_seqs)
 
@@ -23,7 +23,6 @@ for secret in secrets:
   lprice = secret % 10
   prize = next(rsecret) % 10
   price_seq = deque([prize - lprice])
-
   for _ in range(3):
     lprice = prize
     prize = next(rsecret) % 10
@@ -38,12 +37,21 @@ for secret in secrets:
     price_seq.append(prize - lprice)
     price_seq.popleft()
 
+print('calc prices_seqs_u')
 prices_seqs_u = set(chain.from_iterable(s.keys() for s in prices_seqs))
+
+print('calc prices_seqs_sums')
 prices_seqs_sums = {s: sum(ps.get(s, 0) for ps in prices_seqs) for s in prices_seqs_u}
 
+print('calc best_sum')
 best_sum = 0
-for s in prices_seqs_sums.values():
+best_k = None
+for k, s in prices_seqs_sums.items():
   if s > best_sum:
     best_sum = s
+    best_k = k
+
+# 2252
 
 print('best_sum', best_sum)
+print('best_k', best_k)
