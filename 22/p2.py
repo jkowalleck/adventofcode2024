90c8a8749102626cac37a9b9275c53174e327eb7
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-25 10:13:25 +0100

    22
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 22/p2.py
+++ 22/p2.py
@@ -1,3 +1,7 @@
+from collections import deque
+from itertools import chain
+
+
 def mix(value, secret):
   return value ^ secret
 
@@ -21,14 +25,38 @@ def mrand(salt):
     yield secret
 
 
-secrets = map(int, open('kow.txt').read().split('\n'))
-secret2k = []
+secrets = map(int, open('sample2.txt').read().split('\n'))
+prices_seqs = []
 for secret in secrets:
+  _prices_seqs = {}
+  prices_seqs.append(_prices_seqs)
+
   rsecret = mrand(secret)
-  for _ in range(2000-1):
-    next(rsecret)
-  secret2k.append(next(rsecret))
 
-res = sum(secret2k)
-print('res', res)
+  lprice = secret % 10
+  prize = next(rsecret) % 10
+  price_seq = deque([prize - lprice])
 
+  for _ in range(3):
+    lprice = prize
+    prize = next(rsecret) % 10
+    price_seq.append(prize - lprice)
+
+  for _ in range(2000):
+    price_seq_t = tuple(price_seq)
+    if price_seq_t not in _prices_seqs:
+      _prices_seqs[price_seq_t] = prize
+    lprice = prize
+    prize = next(rsecret) % 10
+    price_seq.append(prize - lprice)
+    price_seq.popleft()
+
+prices_seqs_u = set(chain.from_iterable(s.keys() for s in prices_seqs))
+prices_seqs_sums = {s: sum(ps.get(s, 0) for ps in prices_seqs) for s in prices_seqs_u}
+
+best_sum = 0
+for s in prices_seqs_sums.values():
+  if s > best_sum:
+    best_sum = s
+
+print('best_sum', best_sum)
