1051ffeb7cf1722adac3dd81d5067536ec0625b6
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-26 21:27:34 +0100

    23
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 23/p2.py
+++ 23/p2.py
@@ -2,7 +2,7 @@ from itertools import chain, pairwise, combinations
 
 cons = {}
 
-for con in open('sample.txt').read().split('\n'):
+for con in open('kow.txt').read().split('\n'):
   a, b = con.split('-')
   if a in cons:
     cons[a].add(b)
@@ -16,9 +16,15 @@ del a, b
 
 groups = set()
 for f, ts in cons.items():
-  g = {f, *ts}
-  for t in ts:
-    g &= {t, *cons[t]}
-  groups.add(tuple(sorted(g)))
+  for n in range(len(ts), 2, -1):
+    found = False
+    for kn in combinations(ts, n):
+      k = {f, *kn}
+      if all(n + 1 == len({i, *cons[i]} & k) for i in kn):
+        groups.add(tuple(sorted(k)))
+        found = True
+        break
+    if found:
+      break
 
-print('res', groups)
+print('res', ','.join(sorted(max(groups, key=len))))
