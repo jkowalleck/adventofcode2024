22ac6a97ca48edb333c3400f1aa4b53d0b41890f
Jan Kowalleck <jan.kowalleck@gmail.com>
2024-12-26 17:16:25 +0100

    23
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 23/p1.py
+++ 23/p1.py
@@ -1,17 +1,25 @@
+from itertools import chain, pairwise, combinations
 
 cons = {}
-
-print(cons)
 
 for con in open('sample.txt').read().split('\n'):
   a, b = con.split('-')
   if a in cons:
-    cons[a].append(b)
+    cons[a].add(b)
   else:
-    cons[a] = [b]
+    cons[a] = {b, }
   if b in cons:
-    cons[a].append(b)
+    cons[b].add(a)
   else:
-    cons[b] = [a]
+    cons[b] = {a, }
+del a, b
 
+groups = set()
+for f, ts in cons.items():
+  if f[0] != 't':
+    continue
+  for c1, c2 in combinations(ts, 2):
+    if c1 in cons[c2]:
+      groups.add(tuple(sorted([f, c1, c2])))
 
+print('res', len(groups))
