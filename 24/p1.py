5d691edb0decac0d973b72c0f8febdc40c360143
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-01-02 13:26:16 +0100

    24
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ 24/p1.py
@@ -0,0 +1,52 @@
+def g_and(a, b):
+  if a is None or b is None:
+    return None
+  return a & b
+
+
+def g_or(a, b):
+  if a is None or b is None:
+    return None
+  return a | b
+
+
+def g_xor(a, b):
+  if a is None or b is None:
+    return None
+  return a ^ b
+
+
+OPS = {
+  'AND': g_and,
+  'OR': g_or,
+  'XOR': g_xor,
+}
+
+regs = dict()
+grid = set()
+
+init, gates = open('kow.txt').read().split('\n\n')
+for i in init.split('\n'):
+  r, s = i.split(': ')
+  regs[r] = s == '1'
+for g in gates.split('\n'):
+  a, op, b, _, t = g.split(' ')
+  grid.add((a, b, OPS[op], t))
+
+while len(grid):
+  g = grid.pop()
+  a, b, op, t = g
+  if t in regs:
+    continue
+  if a in regs and b in regs:
+    regs[t] = op(regs[a], regs[b])
+  else:
+    grid.add(g)
+
+res = 0
+z_max = max(int(r[1:]) for r in regs if r.startswith('z'))
+for z in range(z_max, 0, -1):
+  if regs[f'z{z:02d}']:
+    res |= 1 << z
+
+print(res)
